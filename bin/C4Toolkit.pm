7ceba233e1c9efc71a8f981d5280cb441c664038
haakon.nessjoen <haakon.nessjoen@4c115661-4a43-0410-b320-a9c8fa4be1f2>
2008-01-14 21:44:18 +0000

    Forgot this

--- bin/C4Toolkit.pm
+++ bin/C4Toolkit.pm
@@ -1,11 +1,39 @@
 package C4Toolkit;
 
+@ISA=qw(Exporter);
+@EXPORT = qw(dbg $dbg);
+@EXPORT_OK = qw();
+use Exporter;
+use Time::HiRes qw(time);
+use strict;
+
+our $dbg = 0;
+my $StartTime = time();
+my $DeviceID;
+
+sub dbg {
+	my $Message = shift;
+	my ($pkg, $file, $line) = caller;
+	my (undef, undef, undef, $subroutine) = caller(1);
+	if ($dbg) {
+		printf(
+			"[%9.3f: % -30s]: %s\n",
+			time() - $StartTime,
+			substr(defined($subroutine) ? $subroutine : $pkg,-30),
+			$Message
+		);
+	}
+}
+
 # Retrieve DeviceID for this computer
 
 sub DeviceID {
+	return $DeviceID if defined $DeviceID;
+
 	my $id = `ifconfig|grep HWaddr|awk '{print \$5}'`;
 	$id =~ s/[^a-f0-9]+//gsi;
-	return "sp-".$id;
+	$DeviceID = 'sp-'.$id;
+	return $DeviceID;
 }
 
 1;
